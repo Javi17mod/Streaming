bfdb9ffa8dfdfff9a3728e47a35ea709a235790d
Javi17mod <123207842+Javi17mod@users.noreply.github.com>
2024-05-03 17:55:32 +0000

    D

--- yt.sh
+++ yt.sh
@@ -1,1 +1,2 @@
-ffmpeg -stream_loop -1 -re -i neffex.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/9kaed9hws98uv -vcodec libx264 -pix_fmt yuvj420p -maxrate 500k -preset ultrafast -r 1 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/{Stream-Key}
+ffmpeg -stream_loop -1 -re -i Video.mov -stream_loop -1 -re -i http://stream.zeno.fm/qkr9sodp7zwtv -vf "scale=1020x720" -vcodec libx264 -pix_fmt yuv420p -maxrate 20k -bufsize 8000k -preset veryfast -r 30 -g 60 -c:a aac -b:a 128k -ar 44100 -strict experimental -b:v 4000k -f flv rtmp://live.twitch.tv/app/live_774875237_IgdovWhKIsTmBZs6riyFu7Mu20udT7
+
